df4c8d10dc67a86c411466e3b710506076ac37a6
Gioele Barabucci <gioele.barabucci@uni-koeln.de>
2013-12-09 11:14:20 +0100

    Basic search form

---
+++ app/controllers/dict_controller.rb
@@ -0,0 +1,9 @@
+class DictController < ApplicationController
+	# GET /dict
+	def index
+	end
+
+	# GET /dict/:id # <- FIXME update to a better path
+	def show
+	end
+end
---
+++ test/helpers/dict_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class DictHelperTest < ActionView::TestCase
+end
