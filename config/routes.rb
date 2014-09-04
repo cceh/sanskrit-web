fd05e0bf22a1474c4a46b0426961e9d85097746a
Gioele Barabucci <gioele.barabucci@uni-koeln.de>
2014-09-04 12:25:13 +0200

    Redirect `.../lemma` and `.../scan` to `.../lemmas` and `.../scans`
    Closes #4.

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Sanskrit::Application.routes.draw do
 	get '/dictionary', :to => redirect('/dictionaries')
 	get '/dictionaries', :to => 'dictionaries#index'
 	resources :dictionary, :controller => 'dictionaries', :only => [:index, :show] do
+		get '/lemma', :to => redirect { |params, req| "/dictionary/#{params[:dictionary_id]}/lemmas" }
 		get '/lemmas', :to => 'lemmas#index'
 		resources :lemma, :controller => 'lemmas', :only => [:index, :show]
 
+		get '/scan', :to => redirect { |params, req| "/dictionary/#{params[:dictionary_id]}/scans" }
 		get '/scans', :to => 'scans#index'
 		resources :scan, :controller => 'scans', :only => [:index, :show]
 	end
