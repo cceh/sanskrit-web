58065c630810f2ade7727d14adcc14f50f07d0b9
Gioele Barabucci <gioele.barabucci@uni-koeln.de>
2014-11-25 11:18:14 +0100

    Start the wrapper with a well-formed document

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 		fRAMEWORK_VARIABLES = [:@_routes, :@_config, :@view_renderer, :@_db_runtime, :@_assigns, :@_controller, :@_request, :@view_flow, :@output_buffer, :@virtual_path, :@haml_buffer, :@marked_for_same_origin_verification]
 		variables = _self.instance_variables - fRAMEWORK_VARIABLES
 
-		wrapper = Nokogiri::XML("<rails:variables xmlns:rails='#{rAILS_NS}'>")
+		wrapper = Nokogiri::XML("<rails:variables xmlns:rails='#{rAILS_NS}'/>")
 
 		if bindings.local_variable_defined?('local_assigns')
 			locals = bindings.local_variable_get('local_assigns')
