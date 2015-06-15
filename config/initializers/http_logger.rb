c0db38d2b28432e3961f0d88ddb085e157f1ec83
Gioele Barabucci <gioele.barabucci@uni-koeln.de>
2015-06-15 16:53:44 +0200

    Configure HttpLogger to run only in the dev environment

--- config/initializers/http_logger.rb
+++ config/initializers/http_logger.rb
@@ -1,5 +1,7 @@
-HttpLogger.log_headers = false  # Default: false
-HttpLogger.log_request_body  = false  # Default: true
-HttpLogger.log_response_body = false  # Default: true
+if Rails.env == 'development'
+	HttpLogger.log_headers = false  # Default: false
+	HttpLogger.log_request_body  = false  # Default: true
+	HttpLogger.log_response_body = false  # Default: true
+end
 
 # Licensed under the ISC licence, see LICENCE.ISC for details
