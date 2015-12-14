f327ae79a66410d02104fa6cd3c6693dcaa197f2
Gioele Barabucci <gioele.barabucci@uni-koeln.de>
2015-12-14 22:04:21 +0100

    Show warning message only after a substantial delay

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,12 +2,16 @@ $(function() {
 	$('details').details();
 
 	$('form input[type=submit]').on('click', function() {
-		var spinner_opts = { left: "85%" };
-		var spinner = new Spinner(spinner_opts).spin();
+		var display_warning_fn = function() {
+			var spinner_opts = { left: "85%" };
+			var spinner = new Spinner(spinner_opts).spin();
 
-		var message_elem = $('#wait-message');
-		message_elem.show();
-		message_elem.append(spinner.el);
+			var message_elem = $('#wait-message');
+			message_elem.show();
+			message_elem.append(spinner.el);
+		};
+
+		setTimeout(display_warning_fn, 1000);
 
 		return true;
 	});
